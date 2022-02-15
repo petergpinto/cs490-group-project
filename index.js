e82cc3ab9e4a866b1abdf07e116ce614ce0ccf29
Peter Pinto <peter@peterpinto.dev>
2022-02-15 11:53:05 -0500

    Added question and testcase insert endpoints

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 let express = require('express');
+let session = require('express-session');
 let app = express();
 let mysql = require('mysql');
 const bluebird = require('bluebird');
@@ -21,13 +22,13 @@ const pool = mysql.createPool({
   connectionLimit : 10,
 });
 
-/*
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-*/
-
+const fiveMinutes = 1000 * 60 * 5;
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: fiveMinutes, sameSite: 'strict' }
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('static'));
@@ -40,21 +41,31 @@ app.post('/login', async function (request, response) {
 	let password = request.body.password;
 
 	if(username && password) {
-		connection.query("SELECT UserId, Username, AccountType FROM Users WHERE Username=? and Password=SHA2(CONCAT(?, Salt), 256)", 
-			[username, password],
-			async function(error, results, fields) {
-				if (error) throw error;
-				if (results.length > 0) {
-					let json = {};
-					json.Result = 'Success';
-					json.UserData = results[0];
-					json.UserData.SessionToken = await getSessionToken(results[0]['UserId']);
-					response.json(json);
-				} else {
-					response.send('Incorrect Username and/or Password!');
-				}
-				response.end();
+
+		checkLoginPromise = () => {
+			return new Promise((resolve, reject)=> {
+				pool.query('SELECT UserId, Username, AccountType FROM Users WHERE Username=? and Password=SHA2(CONCAT(?, Salt), 256)', [username, password],
+					async (error, elements)=> {
+						if(error) return reject(error);
+						let json = {};
+						if(elements.length > 0) {
+							json.Result = 'Success';
+        		            json.UserData = elements[0];
+		                    json.UserData.SessionToken = await getSessionToken(elements[0]['UserId']);
+						} else {
+							json.Result = 'Error';
+						}
+						return resolve(json);
+					});
 			});
+		}
+		let loginResult = await checkLoginPromise();
+		if(loginResult.Result == "Success") {
+			request.session.loggedin = true;
+			request.session.UserData = loginResult.UserData;
+		}
+		response.json(loginResult);
+		response.end();
 	} else {
 		response.send('Please enter Username and Password!');
         response.end();
@@ -81,6 +92,103 @@ app.post('/checkSession', async function (request, response) {
 
 })
 
+app.post('/insertQuestion', async function (request, response) {
+	//Must be logged in as teacher
+	//Must have valid SessionToken in express session
+	//Must have following fields: QuestionText, FunctionName
+
+	if(!(await isUserLoggedIn(request.session))) {
+		response.send('Please login');
+        response.end();
+		return;
+	} 
+
+	let QuestionText = request.body.QuestionText;
+	let FunctionName = request.body.FunctionName;
+	if(!FunctionName || !QuestionText) {
+		response.json({'Result':'Invalid Request'});
+        response.end();
+	} else {
+		insertQuestionPromise = () => {
+			return new Promise((resolve, reject) => {
+				pool.query('INSERT INTO Questions (QuestionText, FunctionName) VALUES (?, ?)', [QuestionText, FunctionName],
+					(error, elements) => {
+						if(error) return reject(error);
+						return resolve(true);
+					});
+			});
+		}
+		if(await insertQuestionPromise()) {
+			response.json({'Result':'Success'});
+			response.end();
+		} else {
+			response.json({'Result':'Error'});
+			response.end();
+		}
+
+	}
+});
+
+app.post('/insertTestCase', async function (request, response) {
+	if(!(await isUserLoggedIn(request.session))) {
+		response.send('Please login');
+		response.end();
+		return;
+	}
+
+	let QuestionId = request.body.QuestionId;
+	let TestCaseInput = request.body.TestCaseInput;
+	let TestCaseOutput = request.body.TestCaseOutput;
+	let TestCaseInputType = request.body.TestCaseInputType; //'S' For strings, 'I' for integers, 'F' for floating point values
+	let TestCaseOutputType = request.body.TestCaseOutputType;
+	if(!QuestionId || !TestCaseInput || !TestCaseOutput || !TestCaseInputType || !TestCaseOutputType) {
+		response.json({'Result':'Invalid Request'});
+		response.end()
+		return;
+	}
+
+	insertTestCasePromise = () => {
+		return new Promise((resolve, reject) => {
+			pool.query('INSERT INTO TestCases (QuestionId, TestCaseInput, TestCaseOutput, TestCaseInputType, TestCaseOutputType) VALUES (?, ?, ?, ?, ?)', [QuestionId, TestCaseInput, TestCaseOutput, TestCaseInputType, TestCaseOutputType],
+				(error, elements) => {
+					if(error) return reject(error);
+					return resolve(true);
+				});
+		});
+	}
+
+	if(await insertTestCasePromise()) {
+		response.json({'Result':'Success'});
+        response.end();
+	} else {
+		response.json({'Result':'Error'});
+        response.end();
+	}
+});
+
+/* Data retrieval endpoints */
+
+app.get('/getAllQuestions', async function(request, response) {
+	//Get all questions in the database in JSON format
+	if(!(await isUserLoggedIn(request.session))) {
+    	response.send("Please login");
+		response.end();
+		return;
+	}
+
+	getAllQuestionsPromise = () => {
+		return new Promise((resolve, reject) => {
+			pool.query('SELECT * FROM Questions', 
+				(error, elements) => {
+					if(error) return reject(error);
+					return resolve(elements);
+				});
+		});
+	}
+	response.json(await getAllQuestionsPromise());
+	response.end();
+});
+
 
 
 
@@ -88,6 +196,22 @@ app.listen(8081, function () {
 	console.log('Node server listening on port 8081!');
 });
 
+
+/* Session Helper functions */
+
+async function isUserLoggedIn(session) {
+	if(!session.loggedin) return false;
+	let checkToken = await checkSessionToken(session.UserData.UserId, session.UserData.SessionToken.Token);
+	if(checkToken.Result != "Success") {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function checkUserRole(session, role) {
+
+}
 
 
 /* Database functions */
@@ -97,12 +221,12 @@ async function getSessionToken(UserId) {
 		return new Promise((resolve, reject)=>{
 			pool.query("REPLACE INTO SessionToken (UserId, Token, InvalidAfter) SELECT ?, SHA2(RAND(), 256), DATE_ADD(NOW(), INTERVAL 15 MINUTE)", [UserId],
 				(error, elements) => {
-					if(error) reject(error);
+					if(error) return reject(error);
 					return resolve(elements);
 				});
 		});
 	}
-	await replaceTokenPromise;
+	await replaceTokenPromise();
 	
 	/*
 	connection.query("REPLACE INTO SessionToken (UserId, Token, InvalidAfter) SELECT ?, SHA2(RAND(), 256), DATE_ADD(NOW(), INTERVAL 15 MINUTE)", [UserId], 
