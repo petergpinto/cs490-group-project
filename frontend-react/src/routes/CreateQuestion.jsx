bf549012bbd4c650e13b5b9a0c2ad9c5d076f084
Peter Pinto <petergpinto@verizon.net>
2022-04-04 15:47:39 -0400

    formatting

--- frontend-react/src/routes/CreateQuestion.jsx
+++ frontend-react/src/routes/CreateQuestion.jsx
@@ -9,6 +9,7 @@ class CreateQuestion extends Component {
 		this.showTestCaseInput = this.showTestCaseInput.bind(this);
 		this.updateButton = this.updateButton.bind(this);
 	}
+
 	updateButton(){
 		this.setState(prevState => ({
 				inputLength: prevState.inputLength+1
@@ -53,43 +54,7 @@ class CreateQuestion extends Component {
 		}
 		//clear form at end
 		questionData.target.reset();
-			/*.then(res => res.json())
-			.then(json => {
-				console.log(json);
-				let test1data = new URLSearchParams();
-				test1data.append('QuestionId', json[0].QuestionId);
-				test1data.append('TestCaseInput', questionData.target[3].value);
-				test1data.append('TestCaseInputType', questionData.target[4].value);
-				test1data.append('TestCaseOutput', questionData.target[5].value);
-                test1data.append('TestCaseOutputType', questionData.target[6].value);
-				fetch('https://cs490backend.peterpinto.dev/insertTestCase', {
-            		method: 'POST',
-            		credentials: 'include',
-           	 		headers: {
-                		'Accept':'application/json',
-                		'content-type':'application/x-www-form-urlencoded'
-            		},
-                		body:test1data
-            	}).then(res => res.json());
-
-				let test2data = new URLSearchParams();
-				test2data.append('QuestionId', json[0].QuestionId);
-                test2data.append('TestCaseInput', questionData.target[7].value);
-                test2data.append('TestCaseInputType', questionData.target[8].value);
-                test2data.append('TestCaseOutput', questionData.target[9].value);
-                test2data.append('TestCaseOutputType', questionData.target[10].value);
-				questionData.target.reset();
-                fetch('https://cs490backend.peterpinto.dev/insertTestCase', {
-                    method: 'POST',
-                    credentials: 'include',
-                    headers: {
-                        'Accept':'application/json',
-                        'content-type':'application/x-www-form-urlencoded'
-                    },
-                        body:test2data
-                }).then(res => res.json());
-			})
-			*/
+		this.setState({ inputLength: 2 });
 	}
 
 	showTestCaseInput() {
@@ -164,7 +129,7 @@ class CreateQuestion extends Component {
 					{ this.showTestCaseInput() }
 					<br />
 					<button onClick={this.updateButton}> Add Test Case</button>
-					<br />
+					<br /><br />
 					<input type= 'submit' name = 'Submit2' id = 'Submit2' value = 'Add Question' />
 				</form>
 					
