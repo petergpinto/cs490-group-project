c182df61000aff844ca14d8ce329526432e1a6c1
Peter Pinto <petergpinto@verizon.net>
2022-04-08 09:46:14 -0400

    fix

--- frontend-react/src/routes/ReviewExams.jsx
+++ frontend-react/src/routes/ReviewExams.jsx
@@ -301,7 +301,7 @@ class ReviewExams extends Component {
 		}
 
 		let items3 = this.state.constraints;
-		for (let i in items2) {
+		for (let i in items3) {
 			if (items3[i].UserId == userId && items3[i].ExamId == this.state.selectedExam && items3[i].CorrectFunctionName == 0 && !(items3[i].QuestionId in small_map)) {
 				points -= 1;
 			}
