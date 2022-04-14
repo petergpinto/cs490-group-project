5902133146b85cb4c801d0f7503bc7e72b6195d3
Peter Pinto <peter@peterpinto.dev>
2022-04-14 12:49:19 -0400

    update

--- frontend-react/src/routes/CreateQuestion.jsx
+++ frontend-react/src/routes/CreateQuestion.jsx
@@ -136,6 +136,7 @@ class CreateQuestion extends Component {
 			return <div>
 				<br/>
 				<label>Test Case {index+1}:</label>
+				<br/><br/>
 				<input type='text' placeholder='Input'/> 
 				<select name='TestCaseInputType' id='TestCaseInputType'>
     				<option value='S'>String</option>
@@ -143,6 +144,7 @@ class CreateQuestion extends Component {
 					<option value='F'>Floating Point</option>
 					<option value='L'>List</option>
 				</select>
+				<br/><br/>
 				<input type='text' placeholder='Output'/>
 				<select name='TestCaseOutputType' id='TestCaseOutputType'>
     				<option value='S'>String</option>
@@ -165,47 +167,47 @@ class CreateQuestion extends Component {
 
 		return (
 			<div className="CreateQuestion">
-				<div style={{'display':'flex', 'flex-direction':'column'}}>
-					<form onSubmit={this.submitQuestion}>
-						<br/>
-						<label>Question Text</label>
-						<br/>
-						<textarea type='text' size='1500' id='QuestionText' key='QuestionText' onChange={this.updateQuestionTextState} value={ this.state.questionText }/>
-						<br/><br/>
-						<label>Function Name</label>
-						<br/>
-						<input type='text' id='FunctionName' key='FunctionName' onChange={this.updateFunctionNameState} value={this.state.functionName}/>
-						<br/><br/>
-						<label>Difficulty Rating </label>
-						<select name='DifficultyRating' onChange={this.updateDifficultyState} value={this.state.difficulty}>
-							<option value='1'>Easy</option>
-							<option value='2'>Medium</option>
-							<option value='3'>Hard</option>
-						</select>
-						<label>Category</label>
-						<select name="Category" onChange={this.updateCategoryState} value={this.state.category}>
-							<option value="For Loops">For Loops</option>
-							<option value="While Loops">While Loops</option>
-							<option value="Basic Math">Basic Math</option>
-							<option value="Recursion">Recursion</option>
-							<option value="Conditionals">Conditionals</option>
-							<option value="Strings">Strings</option>
-							<option value="Advanced Math">Advanced Math</option>
-						</select>
-						<label>Constraint Type</label>
-						<select name="ConstraintType" onChange={this.updateConstraintState} value={this.state.constraint}>
-							<option value="None">None</option>
-							<option value="For">For</option>
-							<option value="While">While</option>
-							<option value="Recursion">Recursion</option>
-						</select>
-						{ this.showTestCaseInput() }
-						<br />
-						<button onClick={this.updateButton} type="button"> Add Test Case</button>
-						<input type='submit' name='Submit2' id='Submit2' value='Add Question' />
-					</form>
-					{this.state.displayCheckmark ? <Icon /> : null}
-				</div>
+				<form onSubmit={this.submitQuestion}>
+					<br/>
+					<label>Question Text</label>
+					<br/>
+					<textarea type='text' size='1500' id='QuestionText' key='QuestionText' onChange={this.updateQuestionTextState} value={ this.state.questionText }/>
+					<br/><br/>
+					<label>Function Name</label>
+					<br/>
+					<input type='text' id='FunctionName' key='FunctionName' onChange={this.updateFunctionNameState} value={this.state.functionName}/>
+					<br/><br/>
+					<label>Difficulty Rating </label>
+					<select name='DifficultyRating' onChange={this.updateDifficultyState} value={this.state.difficulty}>
+						<option value='1'>Easy</option>
+						<option value='2'>Medium</option>
+						<option value='3'>Hard</option>
+					</select>
+					<br/><br/>
+					<label>Category</label>
+					<select name="Category" onChange={this.updateCategoryState} value={this.state.category}>
+						<option value="For Loops">For Loops</option>
+						<option value="While Loops">While Loops</option>
+						<option value="Basic Math">Basic Math</option>
+						<option value="Recursion">Recursion</option>
+						<option value="Conditionals">Conditionals</option>
+						<option value="Strings">Strings</option>
+						<option value="Advanced Math">Advanced Math</option>
+					</select>
+					<br/><br/>
+					<label>Constraint Type</label>
+					<select name="ConstraintType" onChange={this.updateConstraintState} value={this.state.constraint}>
+						<option value="None">None</option>
+						<option value="For">For</option>
+						<option value="While">While</option>
+						<option value="Recursion">Recursion</option>
+					</select>
+					{ this.showTestCaseInput() }
+					<br />
+					<button onClick={this.updateButton} type="button"> Add Test Case</button>
+					<input type= 'submit' name = 'Submit2' id = 'Submit2' value = 'Add Question' />
+				</form>
+				{this.state.displayCheckmark ? <Icon /> : null}
 				<ShowQuestionBank showElement={true} />
 			</div>
 		);
